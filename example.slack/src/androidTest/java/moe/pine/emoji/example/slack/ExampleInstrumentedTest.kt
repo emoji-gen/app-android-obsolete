b12fc322fe9ac5c342cfa2c15647fdca2b29e360
Pine Mizune <pinemz@gmail.com>
2017-04-16 21:43:32 +0900

    test(tests): remove unused tests

---
+++ example.slack/src/androidTest/java/moe/pine/emoji/example/slack/ExampleInstrumentedTest.kt
@@ -0,0 +1,13 @@
+package moe.pine.emoji.example.slack
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+}
---
+++ example.slack/src/test/java/moe/pine/emoji/example/slack/ExampleUnitTest.kt
@@ -0,0 +1,9 @@
+package moe.pine.emoji.example.slack
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+}
