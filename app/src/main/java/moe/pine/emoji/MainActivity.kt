9904082d954cca6a4ced320594f38d608d68a14b
Pine Mizune <pinemz@gmail.com>
2017-04-18 02:45:10 +0900

    feat(splash): add splash activity

--- app/src/main/java/moe/pine/emoji/MainActivity.kt
+++ app/src/main/java/moe/pine/emoji/MainActivity.kt
@@ -1,12 +1,22 @@
 package moe.pine.emoji
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 
+/**
+ * Activity for main
+ * Created by pine on Apr 18, 2017.
+ */
 class MainActivity : AppCompatActivity() {
+    companion object {
+        fun createIntent(context: Context): Intent = Intent(context, MainActivity::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/moe/pine/emoji/SplashActivity.kt
@@ -0,0 +1,26 @@
+package moe.pine.emoji
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Activity for splash
+ * Created by pine on Apr 18, 2017.
+ */
+class SplashActivity : AppCompatActivity() {
+    private val handler: Handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        this.handler.postDelayed({
+            this.startActivity(MainActivity.createIntent(this))
+            this.finish()
+        }, 700)
+    }
+}
