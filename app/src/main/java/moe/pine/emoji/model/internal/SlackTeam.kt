561863d9c811224a77f8816fcb3241f04dba469b
Pine Mizune <pinemz@gmail.com>
2017-05-14 02:47:25 +0900

    feat(Realm): add realm

---
+++ app/src/main/java/moe/pine/emoji/model/internal/SlackTeam.kt
@@ -0,0 +1,16 @@
+package moe.pine.emoji.model.internal
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+
+/**
+ * Model for slack team
+ * Created by pine on May 14, 2017.
+ */
+@RealmClass
+class SlackTeam(
+        @PrimaryKey var domain: String,
+        var email: String,
+        val password: String
+) : RealmObject()
