787ae7cfb5b40d25ba74052e9c68f933d15cc73d
Pine Mizune <pinemz@gmail.com>
2017-06-03 21:58:21 +0900

    fix(TeamSpinnerItemView): add arrow down image

--- app/src/main/java/moe/pine/emoji/view/generator/TeamSpinnerItemView.kt
+++ app/src/main/java/moe/pine/emoji/view/generator/TeamSpinnerItemView.kt
@@ -2,7 +2,7 @@ package moe.pine.emoji.view.generator
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.FrameLayout
+import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.view_generator_team_spinner_item.view.*
 import moe.pine.emoji.model.realm.SlackTeam
 
@@ -10,7 +10,7 @@ import moe.pine.emoji.model.realm.SlackTeam
  * View for team spinner
  * Created by pine on May 14, 2017.
  */
-class TeamSpinnerItemView : FrameLayout {
+class TeamSpinnerItemView : RelativeLayout {
     var team: SlackTeam? = null
         set(value) {
             this.text_view_generator_team_name.text = value?.team.orEmpty()
